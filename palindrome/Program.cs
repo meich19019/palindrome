24591ce940ebb9ac93498bcd40a2d43d4ece6664
梅智皓 <meich19019@gmail.com>
2022-05-27 15:29:47 +0800

    Initial commit

---
+++ palindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("redder"));
+            Console.WriteLine(IsPalindrome("alpha"));
+        }
+
+        public static bool IsPalindrome(string s)
+        {
+            bool result = false;
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            string reverseString = new string(charArray);
+            if (s == reverseString)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
